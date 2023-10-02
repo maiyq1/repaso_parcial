deb2c1e49ae9f7011d212c5c71a4e8bd877fcd82
Mijael Yen Quispe <mijael.yq2002@outlook.com>
2023-10-02 16:43:19 -0500

    feat: Datatable I of III

--- src/components/healthcheck-component.vue
+++ src/components/healthcheck-component.vue
@@ -1,15 +1,30 @@
 <script>
 import technogymtoolbarComponent from "@/components/technogymtoolbar-component.vue";
-
+import {TechnoApiService} from "@/services/techno-data";
 export default {
   name: "healthcheck",
   components: {technogymtoolbarComponent},
+  data() {
+    return {
+      treadData: []
+    }
+  },
+  mounted() {
+    new TechnoApiService().getAllRecords().then((response) => {
+      this.treadData = response.data;
+    })
+  }
 }
 </script>
 
 <template>
-  <h1>HealthCheck view</h1>
-  <p>Card</p>
+  <pv-datatable :value="treadData">
+    <pv-column header="Record Id" field="id"></pv-column>
+    <pv-column header="Treadmill Id" field="treadmillId"></pv-column>
+    <pv-column header="Volts" field="volts"></pv-column>
+    <pv-column header="Watts" field="watts"></pv-column>
+    <pv-column header="HP" field="hp"></pv-column>
+  </pv-datatable>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import PrimeVue from "primevue/config";
 import Toolbar from "primevue/toolbar";
 import Button from "primevue/button";
 import Card from "primevue/card";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 import my_router from "./router";
 
 const app = createApp(App);
@@ -18,5 +20,7 @@ app.use(my_router);
 app.component('pv-toolbar', Toolbar);
 app.component('pv-button', Button);
 app.component('pv-card', Card);
+app.component('pv-datatable', DataTable);
+app.component('pv-column', Column);
 
 app.mount("#app");
